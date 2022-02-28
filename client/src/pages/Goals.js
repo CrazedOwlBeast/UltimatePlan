b7f2113b69c9a4369eb44f16d8f57843d06fe6b6
Anna Le <annale1501@gmail.com>
2022-02-27 23:19:08 -0500

    more ui

--- client/src/pages/Goals.js
+++ client/src/pages/Goals.js
@@ -1,22 +1,72 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import './Goals.css'
 import SharedGoal from '../components/SharedGoal'
+import Dropdown from 'react-dropdown';
 
 const Goals = () => {
 
+  
+  const axios = require('axios');
+
+  const handlePost = () => {
+      axios.put('/share/:id', {
+          // text: 'testing the post',
+          // user: 'testUser'
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+  const handleGoalPost = () => {
+    axios.post('/', {
+        text: 'testing the post',
+        user: 'testUser'
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+  const options = [
+    'one', 'two', 'three'
+  ];
+
+  useEffect(() => {
+    axios.get('/mygoals')
+    .then(function (response) {
+      options = JSON.parse(response);
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  });
 
   return (
     <div className='goals'>
         <div className='goals-left-container'> 
+          <div className='new-goal-container'>
+            <div className='new-goal'>
+             <textarea className='new-goal-input' placeholder='Add new goal'></textarea>
+             <button className='post-goal-btn' onClick={handleGoalPost}>Post</button>
+            </div>
+          </div>
           <div className='goals-container'>
             <div className='goals-content'>
               <div className='goals-category'>
-                Category
+                Goal Category
               </div>
               <ul className='ul'>
-                <li className='goal-item'>Test</li>
-                <li>Test</li>
-                <li>Test</li>
+                <li className='goal-item'>Goal</li>
+                <li className='goal-item'>Goal</li>
+                <li className='goal-item'>Goal</li>
               </ul>
             </div>
           </div>
@@ -24,8 +74,9 @@ const Goals = () => {
         <div className='goals-right-container'>
           <div className='goals-input'>
             <div className='goals-post-content'>
+              <Dropdown options={options} className='dropdown' placeholder='Select a goal' />
               <textarea className='post-input' placeholder='Post an update'></textarea>
-              <button className='post-btn'>Post</button>
+              <button className='post-btn' onClick={handlePost}>Post</button>
             </div>
           </div>
           <div clsasName='feed'>
