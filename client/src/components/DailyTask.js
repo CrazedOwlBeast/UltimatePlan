fcbfda1621796d484f4d5f164184b67ea3110309
CrazedOwlBeast <nicolasmorita15@gmail.com>
2022-03-03 00:55:07 -0500

    daily tasks functionality

--- client/src/components/DailyTask.js
+++ client/src/components/DailyTask.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './DailyTask.css'
 import { Button } from '@mui/material';
 
 const DailyTask = ({Day}) => {
 
+  const [task, setTask] = useState('');
+
+  const handleTask = (e) => {
+    e.preventDefault();
+    
+    var x = 0;
+    switch (Day) {
+      case 'Monday':
+        x = 0;
+        break;
+      case 'Tuesday':
+        x = 1;
+        break;
+      case 'Wednesday':
+        x = 2;
+        break;
+      case 'Thursday':
+        x = 3;
+        break;
+      case 'Friday':
+        x = 4;
+        break;
+      case 'Saturday':
+        x = 5;
+        break;
+      case 'Sunday':
+        x = 6;
+        break;
+      default:
+        break;
+    }
+
+    let list = document.getElementsByClassName('ul')[x];
+    let entry = document.createElement('li');
+    entry.className = 'task-item';
+    entry.appendChild(document.createTextNode(task));
+    list.appendChild(entry);
+
+    setTask('');
+  } 
   
   return (
     <div className='dailytask'>
@@ -14,11 +54,14 @@ const DailyTask = ({Day}) => {
         <div className='form-container'>
           <form className='form'>
             <input className='input' />
-            <button className='submit-btn'>Submit</button>
+            <button className='submit-btn' onClick={handleTask}>Submit</button>
           </form>
         </div>
         <div className='tasks-container'>
           <p className='daily-title'>Tasks</p>
+          <ul className='ul'>
+
+          </ul>
         </div>
       </div>
     </div>
--- client/src/pages/Goals.js
+++ client/src/pages/Goals.js
@@ -1,22 +1,102 @@
-import React from 'react'
+import { createElement, React, useEffect, useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 import './Goals.css'
 import SharedGoal from '../components/SharedGoal'
+import Dropdown from 'react-dropdown';
+
+
+
+
 
 const Goals = () => {
 
+  
+  // const axios = require('axios');
 
+  // const handlePost = () => {
+  //     axios.put('/share/:id', {
+  //         // text: 'testing the post',
+  //         // user: 'testUser'
+  //       })
+  //       .then(function (response) {
+  //         console.log(response);
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error);
+  //       });
+  //   }
+
+  // const handleGoalPost = () => {
+  //   axios.post('/', {
+  //       text: 'testing the post',
+  //       user: 'testUser'
+  //     })
+  //     .then(function (response) {
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  //   }
+
+
+  // useEffect(() => {
+  //   axios.get('/mygoals')
+  //   .then(function (response) {
+  //     options = JSON.parse(response);
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   })
+  // });
+  
+
+  const [options, setOptions] = useState(['one', 'two', 'three']);
+  const [goal_text, setGoalText] = useState('');
+
+  const handleNewGoal = (e) => {
+    console.log('didnt get here');
+    let list = document.getElementsByClassName('ul')[0];
+    let entry = document.createElement('li');
+    entry.className = 'goal-item';
+    entry.appendChild(document.createTextNode(goal_text));
+    list.appendChild(entry);
+
+    const newList = options.concat(goal_text);
+    setOptions(newList);
+
+    setGoalText('');
+  } 
+
+  const handlePost = (e) => {
+    
+  }
+  
   return (
     <div className='goals'>
         <div className='goals-left-container'> 
+          <div className='new-goal-container'>
+            <div className='new-goal'>
+             <textarea 
+              className='new-goal-input' 
+              placeholder='Add new goal'
+              value={goal_text}
+              onChange={(e) => setGoalText(e.target.value)}
+              ></textarea>
+             <button className='post-goal-btn' onClick={handleNewGoal}>Post</button>
+            </div>
+          </div>
           <div className='goals-container'>
             <div className='goals-content'>
               <div className='goals-category'>
-                Category
+                Goal Category
               </div>
-              <ul className='ul'>
-                <li className='goal-item'>Test</li>
-                <li>Test</li>
-                <li>Test</li>
+              <ul className='ul' id='my-goals'>
+
+                <li className='goal-item'>Goal</li>
+                <li className='goal-item'>Goal</li>
+                <li className='goal-item'>Goal</li>
               </ul>
             </div>
           </div>
@@ -24,17 +104,19 @@ const Goals = () => {
         <div className='goals-right-container'>
           <div className='goals-input'>
             <div className='goals-post-content'>
+              <Dropdown options={options} className='dropdown' placeholder='Select a goal' />
               <textarea className='post-input' placeholder='Post an update'></textarea>
-              <button className='post-btn'>Post</button>
+              <button className='post-btn' onClick={handlePost}>Post</button>
             </div>
           </div>
-          <div clsasName='feed'>
+          <div className='feed'>
             <SharedGoal user='User' goal='This is a random goal' update='This is a random update for the goal'/>
             <SharedGoal user='User' goal='This is a random goal' update='This is a random update for the goal'/>
             <SharedGoal user='User' goal='This is a random goal' update='This is a random update for the goal'/>
           </div>
         </div>
     </div>
+    
   )
 }
 
