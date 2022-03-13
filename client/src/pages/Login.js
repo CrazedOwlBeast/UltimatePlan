1929d2d70ef0c1226119d9bf2eb0dca6737afca8
CrazedOwlBeast <nicolasmorita15@gmail.com>
2022-03-12 21:15:44 -0500

    friend list

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Login.css'
 
 const Login = () => {
+  
   return (
     <div className='login'>
         <div className='login-container'>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Profile.css'
 
 const Profile = () => {
+
+    const [friendsList, setFriendsList] = useState(['Jane Doe']);
+    const [friend, setFriend] = useState('');
+
+    const handleFriends = (e) => {
+        e.preventDefault();
+        const newList = friendsList.concat(friend);
+        setFriendsList(newList);
+        setFriend('');
+    }
+
+
   return (
     <div className='profile'>
         <div className='profile-container'>
@@ -17,7 +29,23 @@ const Profile = () => {
                     <div className='bottom-content'>
                         <p>Email</p>
                         <p>Friends</p>
+                        <div className='friends-list'>
+                            <ul>
+                                {friendsList.map((friend) => (
+                                    <li>{friend}</li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
+                </div>
+                <div className='friend-form'>
+                <form className='form'>
+                    <input className='input' 
+                    value={friend}
+                    onChange={(e) => setFriend(e.target.value)}
+                    />
+                    <button className='submit-btn' onClick={handleFriends}>Submit</button>
+                </form>
                 </div>
             </div>
         </div>
